908191d367e046e14e95ec1db525a898a542fda2
Menta100 <83983018+Menta100@users.noreply.github.com>
2021-06-27 11:28:55 +0300

    Add files via upload

---
+++ Component.py
@@ -0,0 +1,7 @@
+class component:
+    def __init__(self, quantity, measure, product):
+        self.quantity = quantity
+        self.measure = measure
+        self.product = product
+
+
---
+++ recipe.py
@@ -0,0 +1,7 @@
+class recipe:
+    def __init__(self, name, components, instructions, tip):
+        self.name = name
+        self.components = components
+        self.instructions = instructions
+        self.tip=tip
+
