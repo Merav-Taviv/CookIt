3c1979c386f918dd742405fe5fca2f646f88fdca
Menta100 <meravx.taviv@intel.com>
2021-11-16 14:42:37 +0200

    Update new2.c

--- new2.c
+++ new2.c
@@ -1,5 +1,4 @@
 #include <stdio.h>#include <stdlib.h>int main(){
-   int num;
    FILE *fptr;
 
    // use appropriate location if you are using MacOS or Linux
@@ -13,7 +12,6 @@
    printf("Enter num: ");
    scanf("%d",&num);
 
-   fprintf(fptr,"%d",num);
    fclose(fptr);
 
    return 0;
