03e19786a5a8289e84fd486fa746d6110a71a7d0
Menta100 <meravx.taviv@intel.com>
2021-11-16 15:36:54 +0200

    upd

--- new1.c
+++ new1.c
@@ -5,11 +5,7 @@
    // use appropriate location if you are using MacOS or Linux
    fptr = fopen("C:\\program.txt","w");
 
-   if(fptr == NULL)
-   {
-      printf("Error!");   
-      exit(1);             
-   }
+   //
 
    printf("Enter num: ");
    scanf("%d",&num);
